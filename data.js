0337fbd0c97b712d1e2d5a784dfde6cd426fc658
Nicolas Germeaux <nicolas@monastudio.co>
2022-07-11 16:01:50 +0200

    Final

---
+++ data.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        id: 1,
+        name: 'User1',
+        email: 'user1@app.org',
+        password: 'react-authentication123',
+    }
+]
---
+++ pages/api/user.js
@@ -0,0 +1,25 @@
+import data from '../../data'
+
+const allowedMethods = ['GET']
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req, res) => {
+    res.setHeader('Allow', allowedMethods)
+
+    if (!allowedMethods.includes(req.method)) {
+        return res.status(405).end()
+    }
+
+    const name = req.cookies.authorization;
+
+    if (!name) {
+        return res.status(404).end()
+    }
+
+    const { email } = data.find(user => user.name === name)
+
+    return res.status(200).json({
+        name,
+        email
+    })
+}
